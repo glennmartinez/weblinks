73effff8d4a2f54459acae8b04ef0652aababdc7
Glenn Martin <glennmartin76@gmail.com>
2014-10-19 15:52:03 +1100

    some changes

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,6 +9,9 @@ Bundler.require(*Rails.groups)
 
 module Weblinks
   class Application < Rails::Application
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
